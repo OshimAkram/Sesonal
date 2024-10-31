d85c391f11c0f10668292fbae849eb1dc6cda3c7
OshimAkram <oshimakram7@gmail.com>
2024-10-31 22:25:38 +0600

    Update Sesonal_Sales_Prediction.py

--- Sesonal_Sales_Prediction.py
+++ Sesonal_Sales_Prediction.py
@@ -4,19 +4,6 @@ import pandas as pd
 import datetime
 from datetime import date,timedelta
 
-
-
-
-st.title("Seasonal Sales Data")
-excel_file='Hot_Winter_Sales.xlsx'
-sheet_name='Sheet1'
-
-df = pd.read_excel(excel_file,
-                  sheet_name=sheet_name,
-                   usecols='B:C',
-                   header=1)
-st.dataframe(df)
-st.title("Seasonal Sales Prediction")
 loaded_model = pickle.load(open('D:/Monthly_Sales_Prediction/seasonalpklt5.sav', 'rb'))
 
 Start_Date = st.date_input("Start Date", date(2025, 1, 1))
